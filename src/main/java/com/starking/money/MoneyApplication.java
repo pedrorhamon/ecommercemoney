2b6d8d0375a891c8eaf17c56a00ec9c7d7740768
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-10-31 20:59:56 -0300

    Project created ecommerce

---
+++ src/main/java/com/starking/money/MoneyApplication.java
@@ -0,0 +1,13 @@
+package com.starking.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyApplication.class, args);
+	}
+
+}
