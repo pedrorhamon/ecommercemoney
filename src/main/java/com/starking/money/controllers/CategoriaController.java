b31f94a366ea1f98a12dd83ea2c10ab3202ce889
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-02 00:41:59 -0300

    cadastrando-nova-categoria-com-POST

--- src/main/java/com/starking/money/controllers/CategoriaController.java
+++ src/main/java/com/starking/money/controllers/CategoriaController.java
@@ -1,11 +1,16 @@
 package com.starking.money.controllers;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.starking.money.model.Categoria;
 import com.starking.money.repositories.CategoriaRepository;
@@ -20,5 +25,21 @@ public class CategoriaController {
 	@GetMapping
 	public List<Categoria> listar() {
 		return this.categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalvar = this.categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+		.buildAndExpand(categoriaSalvar.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalvar);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(Categoria categoria) {
+		return this.categoriaRepository.findById(categoria.getCodigo());
 	}
 }
--- src/main/java/com/starking/money/model/Categoria.java
+++ src/main/java/com/starking/money/model/Categoria.java
@@ -14,7 +14,7 @@ import lombok.*;
 public class Categoria {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
 	private String nome;
