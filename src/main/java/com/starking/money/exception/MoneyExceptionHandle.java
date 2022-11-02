cb295c47d0c36d3a5628fea8a4740c5ef9c0034d
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-02 01:25:55 -0300

    tratando-erros-com-exception-handler

---
+++ src/main/java/com/starking/money/exception/MoneyExceptionHandle.java
@@ -0,0 +1,38 @@
+package com.starking.money.exception;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@ControllerAdvice
+public class MoneyExceptionHandle extends ResponseEntityExceptionHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDev = ex.getCause().toString();
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDev), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@Data
+	@AllArgsConstructor
+	public static class Erro {
+		private String mensagemUsuario;
+		private String mensagemDev;
+	}
+}
