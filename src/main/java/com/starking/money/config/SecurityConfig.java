144bc1dfc02f67b0e2a8dfcad4e401be7e7c1365
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-11-06 17:18:36 -0300

    configurando refresh do jwt

--- src/main/java/com/starking/money/config/SecurityConfig.java
+++ src/main/java/com/starking/money/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.starking.money.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -35,5 +38,11 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.stateless(true);
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();    
 	}
 }
